f713ad06400b4c3ff8520fcda617778e1ec38237
Abdeldjallil Hadji <ncdjilou09@gmail.com>
2021-06-19 23:28:16 +0100

    tweaks

--- tudorwatch/scraper.py
+++ tudorwatch/scraper.py
@@ -26,21 +26,28 @@ try:
             button = driver.find_element_by_css_selector(".sc-llYToB")
         except:
             break
-
     a_tags = driver.find_elements_by_css_selector(".sc-cvZATX")
     for a in a_tags:
-        url = a.get_attribute('href')
-        res = requests.get(url)
-        print(f'Scrapping URL : ' + url)
-        soup = BeautifulSoup(res.text, 'html.parser')
-        title = ' : '.join([t.text for t in soup.findAll('a', class_='breadcrumb-link')[2:4]])
-        elements = soup.find('div', class_='watchspecifications').findChildren('li')
-        specs = '\n'.join([e.findChildren("h3")[0].text + " : " + e.findChildren("p")[0].text for e in elements if len(e.findChildren("h3")) and len(e.findChildren("p"))])
-        watches_count = watches_count + 1 
-        watches[watches_count] = [url, title, specs]
-        items_df = pd.DataFrame.from_dict(watches, orient='index', columns = ['URL' , 'Title', 'Specifications'])
-        items_df.to_csv('tudor.csv')  
-        print('------------------------------------------------------------------------------------------------------------')
+        try :
+            url = a.get_attribute('href')
+            res = requests.get(url)
+            print(f'Scrapping URL : ' + url)
+            soup = BeautifulSoup(res.text, 'html.parser')
+            title = ' : '.join([t.text for t in soup.findAll('a', class_='breadcrumb-link')[2:4]])
+            elements = soup.find('div', class_='watchspecifications').findChildren('li')
+            titles = []
+            descs = []
+            for e in elements[1:]:
+                if len(e.findChildren("h3")) and len(e.findChildren("p")):
+                    titles.append(e.findChildren("h3")[0].text)
+                    descs.append(e.findChildren("p")[0].text)        
+            watches_count = watches_count + 1 
+            watches[watches_count] = [url, title] + descs
+            items_df = pd.DataFrame.from_dict(watches, orient='index', columns = ['URL' , 'Title'] + titles)
+            items_df.to_csv('tudor.csv')  
+            print('------------------------------------------------------------------------------------------------------------')
+        except: 
+            continue
 except:
     print("Something went wrong run the scrapper again please.")
 
